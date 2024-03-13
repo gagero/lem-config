c68ba7368018eef77b9d29eca3ca43a98a21e526
Gabor Gero <gaga.gero@gmail.com>
2024-03-13 15:55:34 +0100

    initial commit

---
+++ init.lisp
@@ -0,0 +1,6 @@
+(in-package :lem-user)
+
+;; match parens, popup autocomplete, which-key, hideshow, (marginalia), snippets, undo-tree, list search results
+
+(define-key *global-keymap*
+  "C-M-Space" 'show-context-menu)
